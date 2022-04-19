393575c6bc1c0504922e8ab4d8f1c208232129ad
furkancgrtkn <furkan.cigirtkan@gmail.com>
2022-04-19 12:30:55 +0300

    fix: images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "kocaelikffcms.herokuapp.com"],
+    domains: [
+      "images.unsplash.com",
+      "kocaelikffcms.herokuapp.com",
+      "141.136.44.174",
+    ],
   },
   env: {
     NEXT_APP_API_URL: "http://141.136.44.174:3000/api/graphql",
