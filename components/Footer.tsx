727d8cd6af1bfe3ced6a649fb347926d2dc40ca8
furkancgrtkn <furkan.cigirtkan@gmail.com>
2022-04-25 01:35:34 +0300

    fixs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -55,7 +55,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-base text-center text-gray-400">
-            &copy; 2022 Kocaleli Film Festivali
+            2022 &copy; Kocaleli Film Festivali
           </p>
         </div>
       </div>
